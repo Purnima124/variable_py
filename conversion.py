31d3d317bbfd9c3242af5380ee332ec3074a1717
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-11 20:12:41 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,7 @@
+# conversion
+var56="20"
+var57=20
+print(type(var57))
+
+
+
---
+++ string.py
@@ -0,0 +1,20 @@
+a="purnima"
+b="19"
+print(a+b)
+
+print(a+"b")
+
+# var23=3.0
+# var24=int(var23)
+# print(type(var24))
+
+# a="@@*************@@"
+# print(a,"@@@@@@@@")
+# print(a,"@@@@@@@")
+
+# a="purnima"
+# b=1997
+# b=str(b)
+# print(a,b)
+
+
